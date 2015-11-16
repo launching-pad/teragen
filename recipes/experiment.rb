3c8f3d9959a52d4ee873768d7c68d0d4e09b5cf3
shelan <shelanrc@gmail.com>
2015-11-16 13:59:52 +0100

    changing chef

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -1,4 +1,11 @@
 
+directory '/srv/teragen' do
+  owner node['wordcount']['user']
+  group node['wordcount']['group']
+  mode '0755'
+  action :create
+end
+
 script 'run_experiment' do
   cwd "/tmp"
   user node['wordcount']['user']
@@ -6,9 +13,7 @@ script 'run_experiment' do
   interpreter "bash"
   code <<-EOM
 
-mkdir ~/teragen
-
-/srv/hadoop/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar teragen 1000 ~/teragen
+/srv/hadoop/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar teragen 10 /srv/teragen
   EOM
 end
 
