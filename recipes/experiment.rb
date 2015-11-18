cbef7bf4aaa1fc6dd0ce012f2057554e461a3d14
Shelan Perera <shelanrc@gmail.com>
2015-11-18 19:24:49 +0100

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -13,7 +13,7 @@ script 'run_experiment' do
   interpreter "bash"
   code <<-EOM
 
-/srv/hadoop/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar teragen 100 /srv/teragen
+/srv/hadoop-2.4.0/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar teragen 100 /srv/teragen
   EOM
 end
 
