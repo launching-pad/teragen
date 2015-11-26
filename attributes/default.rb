f37825afdada06fd9358c9318d13f505602072fd
Shelan Perera <shelanrc@gmail.com>
2015-11-26 11:58:41 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # 'user' and 'group' define the unix user and group, respectively, 
 # that the experiment will be excecuted as.
-default[:wordcount][:group] = "wordcount"
-default[:wordcount][:user] = "wordcount"
+default[:wordcount][:group] = "vagrant"
+default[:wordcount][:user] = "vagrant"
 
