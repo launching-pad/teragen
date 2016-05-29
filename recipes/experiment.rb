8a246043a32f313d6241ee780a2962f31665fdfc
Shelan Perera <shelanrc@gmail.com>
2016-05-29 18:00:45 +0200

    Update experiment.rb

--- recipes/experiment.rb
+++ recipes/experiment.rb
@@ -7,7 +7,7 @@ script 'run_experiment' do
   code <<-EOM
 /srv/hadoop/bin/hadoop fs -rmr /srv
 /srv/hadoop-2.4.0/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar teragen #{node[:teragen][:records]} /srv/teragen
-/srv/hadoop-2.4.0/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar terasort /srv/teragen/output
+/srv/hadoop-2.4.0/bin/hadoop jar /srv/hadoop-2.4.0/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar terasort /srv/teragen /srv/teragen/output
   EOM
 end
 
